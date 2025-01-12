7159c9767d0c752d4e9f87af3665341ecd563cda
Евгений <@DESKTOP-F655TB1>
2025-01-12 22:20:18 +0300

    IP-Calc

--- IPcalc/Main.cpp
+++ IPcalc/Main.cpp
@@ -1,6 +1,9 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include<Windows.h>
 #include<CommCtrl.h>
+#include<cstdio>
 #include"resource1.h"
+
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
@@ -15,12 +18,68 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 	case WM_INITDIALOG:
 	{
+		//InitCommonControls();
 		HWND hUpDown = GetDlgItem(hwnd, IDC_SPIN_PREFIX);
 		SendMessage(hUpDown, UDM_SETRANGE, 0, MAKELPARAM(30,1));
 		
 	}
 		break;
 	case WM_COMMAND:
+	{
+		HWND hIPaddress = GetDlgItem(hwnd, IDC_IPADDRESS);
+		HWND hIPmask = GetDlgItem(hwnd, IDC_IPMASK);
+		HWND hEditPrefix = GetDlgItem(hwnd, IDC_EDIT_PREFIX);
+		DWORD dwIPaddress = 0;
+		DWORD dwIPmask = 0;
+		CHAR sz_prefix[3]{};
+		CONST INT SIZE = 256;
+		switch (LOWORD(wParam))
+		{
+		case IDC_IPADDRESS:
+		{
+			if (HIWORD(wParam) == EN_CHANGE)
+			{
+				SendMessage(hIPaddress, IPM_GETADDRESS, 0, (LPARAM)&dwIPaddress);
+				BYTE first = FIRST_IPADDRESS(dwIPaddress);
+				if (first < 128)dwIPmask = MAKEIPADDRESS(255, 0, 0, 0);
+				else if (first < 192)dwIPmask = MAKEIPADDRESS(255, 255, 0, 0);
+				else if (first < 224)dwIPmask = MAKEIPADDRESS(255, 255, 255, 0);
+				SendMessage(hIPmask, IPM_SETADDRESS, 0, dwIPmask);
+			}
+		}
+		break;
+		case IDC_IPMASK:
+		{
+			if (HIWORD(wParam) == EN_CHANGE)
+			{
+				SendMessage(hIPmask, IPM_GETADDRESS, 0, (LPARAM) & dwIPmask);
+				int i = 1;
+				for (; dwIPmask <<= 1; i++);
+				sprintf(sz_prefix, "%i", i);
+				SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)sz_prefix);
+
+			}
+		}
+		break;
+		case IDC_EDIT_PREFIX:
+		{
+			SendMessage(hEditPrefix, WM_GETTEXT, 3, (LPARAM)sz_prefix);
+			DWORD dwIPprefix = UINT_MAX;
+
+		}
+		break;
+		case IDOK:
+		{
+			
+			CHAR sz_info[SIZE] = "Info:\nЗдесь будет информация о сети"; //sz - String Zero (NULL - Terminated Line)
+			HWND hInfo = GetDlgItem(hwnd, IDC_STATIC_INFO);
+			SendMessage(hInfo, WM_SETTEXT, 0, (LPARAM)sz_info);
+		}
+		break;
+		case IDCANCEL: EndDialog(hwnd, 0);
+			break;
+		}
+	}
 		break;
 	case WM_CLOSE:
 		EndDialog(hwnd, 0);
